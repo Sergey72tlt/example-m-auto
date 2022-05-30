4d3bcb7bd53bb8fd015375386a7bd90882814259
Sergey72tlt <94053785+Sergey72tlt@users.noreply.github.com>
2022-05-30 21:17:41 +0400

    add tests

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from core.models import Profile
 from django.urls import reverse
+import codecs
+from django.core.files.uploadedfile import SimpleUploadedFile
 from ..models import Post
 
 
@@ -10,6 +12,16 @@ class TestFeedView(TestCase):
     def setUp(self):
         dir = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(dir, 'static', 'img', 'testimage.jpg')
-        self.test.user1 = User.objects.create(username='test1', email='test@test.com', password='test')
-        self.test.user2 = User.objects.create(username='test2', email='test1@test.com', password='test')
+        file_obj = codecs.open(image_path, encoding='base64')
+        image = SimpleUploadedFile(file_obj.name, file_obj.read())
+
+        self.test_user1 = User.objects.create(username='test1', email='test@test.com', password='test')
+        self.test_user2 = User.objects.create(username='test2', email='test1@test.com', password='test')
+
+        post = Post.objects.create(author=self.test_user1, image=image)
         return super().setUp()
+
+    def test_feed_blank_content(self):
+        self.client.login(username='test1', password='test')
+        response = self.client.get(reverse('posts:index'))
+        self.assertIn('posts', response.context)
